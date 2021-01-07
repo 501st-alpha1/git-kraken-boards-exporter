b066acb02aabb9cbe1ac74f161d281116b958261
Scott Weldon <open-source@scott-weldon.com>
2021-01-06 22:11:13 -0800

    Get boards and write to file

--- export.py
+++ export.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
 import os
+import jsonpickle
 from py_glo_boards_api import GloBoard, types
 
 token = os.environ['GIT_KRAKEN_PERSONAL_TOKEN']
 
 globoard = GloBoard(token)
+
+boards = globoard.get_boards()
+with open('boards.json', 'w') as file:
+  file.write(jsonpickle.encode(boards, unpicklable=False))
